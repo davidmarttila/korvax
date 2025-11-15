e7ed76d4e675a251867e5f70e2395e1fd84ec004
David Marttila <d.sudholt@qmul.ac.uk>
2025-11-15 02:13:30 +0000

    add sot benchmark

--- scripts/benchmarks/loss.py
+++ scripts/benchmarks/loss.py
@@ -11,13 +11,15 @@ import click
 
 from time import time
 
+from sot import Wasserstein1DLoss
+
 
 def block_jax(fn, *args, **kwargs):
     fn(*args, **kwargs).block_until_ready()
 
 
 @jax.jit
-def jax_grads(x, y):
+def jax_mrstft_grads(x, y):
     return jax.grad(jax_mrstft, argnums=0)(x, y)
 
 
@@ -44,14 +46,63 @@ torch_mrstft = auraloss.freq.MultiResolutionSTFTLoss(
 torch_mrstft = torch.compile(torch_mrstft)
 
 
+jax_sot = jax.jit(
+    partial(
+        korvax.loss.time_frequency_loss,
+        transform_fn=partial(
+            korvax.spectrogram,
+            hop_length=512,
+            win_length=2048,
+            window="flattop",
+            power=2,
+        ),
+        loss_fn=partial(
+            korvax.loss.spectral_optimal_transport_loss,
+            balanced=True,
+            normalize=True,
+            limit_quantile_range=False,
+            p=2,
+        ),
+    )
+)
+
+torch_sot = Wasserstein1DLoss(
+    transform="stft",
+    hop_length=512,
+    fft_size=2048,
+    window="flattop",
+    square_magnitude=True,
+    sample_rate=16000,
+    balanced=True,
+    normalize=True,
+    quantile_lowpass=False,
+    p=2,
+)
+
+torch_sot = torch.compile(torch_sot)
+
+
+@jax.jit
+def jax_sot_grads(x, y):
+    return jax.grad(jax_sot, argnums=0)(x, y)
+
+
+@torch.compile
+def torch_sot_grads(x, y):
+    loss = torch_sot(x, y)
+    loss.backward()
+    return x.grad
+
+
 # this made performance 4x worse!!
 # torch._dynamo.config.compiled_autograd = True
 
 
 @torch.compile
-def torch_grads(x, y):
+def torch_mrstft_grads(x, y):
     loss = torch_mrstft(x, y)
     loss.backward()
+    return x.grad
 
 
 def run_benchmark(func, *args, runs: int = 100, **kwargs):
@@ -90,18 +141,32 @@ def main(device, batch_size, length, seed, runs, precision):
     )
 
     korvax_time = run_benchmark(partial(block_jax, jax_mrstft), x, y, runs=runs)
-    print(f"Korvax: {korvax_time * 1000:.3f} ms")
+    print(f"Korvax MRSTFT: {korvax_time * 1000:.3f} ms")
 
     torch_time = run_benchmark(torch_mrstft, x_torch, y_torch, runs=runs)
-    print(f"Torch: {torch_time * 1000:.3f} ms")
+    print(f"Torch MRSTFT: {torch_time * 1000:.3f} ms")
 
-    korvax_grad_time = run_benchmark(partial(block_jax, jax_grads), x, y, runs=runs)
-    print(f"Korvax grads: {korvax_grad_time * 1000:.3f} ms")
-
+    korvax_grad_time = run_benchmark(
+        partial(block_jax, jax_mrstft_grads), x, y, runs=runs
+    )
+    print(f"Korvax MRSTFT grads: {korvax_grad_time * 1000:.3f} ms")
     x_torch = x_torch.requires_grad_(True)
 
-    torch_grad_time = run_benchmark(torch_grads, x_torch, y_torch, runs=runs)
-    print(f"Torch grads: {torch_grad_time * 1000:.3f} ms")
+    torch_grad_time = run_benchmark(torch_mrstft_grads, x_torch, y_torch, runs=runs)
+    print(f"Torch MRSTFT grads: {torch_grad_time * 1000:.3f} ms")
+
+    korvax_time = run_benchmark(partial(block_jax, jax_sot), x, y, runs=runs)
+    print(f"Korvax SOT: {korvax_time * 1000:.3f} ms")
+
+    torch_time = run_benchmark(torch_sot, x_torch, y_torch, runs=runs)
+    print(f"Torch SOT: {torch_time * 1000:.3f} ms")
+
+    korvax_grad_time = run_benchmark(partial(block_jax, jax_sot_grads), x, y, runs=runs)
+    print(f"Korvax SOT grads: {korvax_grad_time * 1000:.3f} ms")
+    x_torch = x_torch.requires_grad_(True)
+
+    torch_grad_time = run_benchmark(torch_sot_grads, x_torch, y_torch, runs=runs)
+    print(f"Torch SOT grads: {torch_grad_time * 1000:.3f} ms")
 
 
 if __name__ == "__main__":
